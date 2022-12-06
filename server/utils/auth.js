be1ea07d2f85e7ba658932c9f098dbb3f31377d3
Capt-Turner <sethhet@gmail.com>
2022-12-06 14:42:07 -0700

    attempting to fix app error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,34 +5,29 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({req}) {
-    // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authroization;
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      req.user = data; // this saves the user data to the context object
     } catch {
-      console.log('Invalid token');
+      console.log("Invalid token");
     }
 
-    // send to next endpoint
     return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
 
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
