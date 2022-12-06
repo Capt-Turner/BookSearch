6d5adfd726cfcde3147fa50a5fe663085e5fd445
Capt-Turner <sethhet@gmail.com>
2022-12-06 15:30:13 -0700

    fixed app.get route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
+    res.sendFile(path.join(__dirname,'../client/public/index.html'))
 });
 
 const startApolloServer=async(typeDefs,resolvers)=>{
