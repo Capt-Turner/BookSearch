264952f46a020668bec17bc054eea0bd88ab98eb
Capt-Turner <sethhet@gmail.com>
2022-12-06 13:34:36 -0700

    added apollo import to signupformjs

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import {useMutation} from '@apollo/client'; 
 import {ADD_USER} from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -35,7 +35,7 @@ const SignupForm = () => {
       });
 
       const {token,user}= data.addUser;
-      
+
       console.log(user);
       Auth.login(token);
     } catch (err) {
