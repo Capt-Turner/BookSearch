103a1189ffb1a326deab99263cc25473ab35f171
Capt-Turner <sethhet@gmail.com>
2022-12-06 15:37:55 -0700

    another attempt at fixing server route

--- server/server.js
+++ server/server.js
@@ -16,8 +16,12 @@ const server=new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/public/index.html'))
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../client/build')));
+};
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../client/build/index.html'));
 });
 
 const startApolloServer=async(typeDefs,resolvers)=>{
