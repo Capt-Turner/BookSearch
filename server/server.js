ae3a4e9139a64dfa50afe6a542bce0f6439a7624
Capt-Turner <sethhet@gmail.com>
2022-12-06 13:55:01 -0700

    added authmiddleware to server.js

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const {ApolloServer} =require('apollo-server-express');
 const {typeDefs,resolvers}=require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 const {authMiddleware}=require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server=new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
 });
 
 app.use(express.urlencoded({ extended: true }));
