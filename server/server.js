b607451c7dd6a2d87119fc3e7ebec93a79faed41
Capt-Turner <sethhet@gmail.com>
2022-12-06 20:06:11 -0700

    attempt 9000000

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const {authMiddleware}=require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server=new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-});
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,6 +18,12 @@ if(process.env.NODE_ENV==='production'){
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
+
+const server=new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
 });
 
 const startApolloServer=async(typeDefs,resolvers)=>{
